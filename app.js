775ed901fb3427e323ebedcbf45eec66e26c64ad
JoyMarkarian <jmarkarian@hotmail.fr>
2022-11-10 10:33:20 +0100

    Création de la route POST

--- app.js
+++ app.js
@@ -4,23 +4,26 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.APP_PORT ?? 2000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
-
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
-
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
 const userHandlers = require("./userHandlers");
-
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+
+app.post("/api/movies", movieHandlers.postMovie);
+
+app.post("/api/users", userHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
