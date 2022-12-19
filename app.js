5d6f20e95765797b258a68295651f7fb53f76289
JoyMarkarian <jmarkarian@hotmail.fr>
2022-12-19 10:43:03 +0100

    Création des routes session-in et session-out

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 
+const path = require ("path");
 const express = require("express");
+const Session = require('express-session');
+const FileStore = require('session-file-store')(Session);
 const { hashPassword, verifyPassword, verifyToken, verifyPayload } = require("./auth");
 const userHandlers = require("./userHandlers");
 const movieHandlers = require("./movieHandlers");
@@ -8,15 +11,34 @@ const movieHandlers = require("./movieHandlers");
 const app = express();
 
 app.use(express.json());
+app.use(Session({
+  store: new FileStore({
+      path: path.join(__dirname, '/tmp'),
+      encrypt: true
+  }),
+  secret: 'Super Secret !',
+  resave: true,
+  saveUninitialized: true,
+  name : 'sessionId'
+}));
 
-const port = process.env.APP_PORT ?? 5000;
-
+const port = process.env.APP_PORT ?? 5002;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
 app.get("/", welcome);
+
+app.get("/api/session-in", (req,res) => {
+  req.session.song = "Be bop a lula";
+  console.log("Test");
+  res.send("Session variable change");
+});
+
+app.get("/api/session-out", (req,res) => {
+  res.send(req.session.song);
+});
 
 // the public routes
 
@@ -35,7 +57,7 @@ app.post(
 
 // then the routes to protect
 
-app.use(verifyToken); // authentication wall : verifyToken is activated for each route after this line
+// app.use(verifyToken); // authentication wall : verifyToken is activated for each route after this line
 
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
